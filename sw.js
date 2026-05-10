10861cedacdf706e7e50cc0e2cdaa287c9cd2b1e
Michel Detiilli <detiillimichel@gmail.com>
2026-05-10 19:43:45 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'calc-v1';
+const assets = ['./', './index.html', './css/style.css', './js/main.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
